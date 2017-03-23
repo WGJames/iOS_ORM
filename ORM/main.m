16fd9122e9729486068934384d04712d52e36d8a
程成 <chengcheng@cashbus.com>
2017-03-23 10:26:05 +0800

    Initial Commit

---
+++ ORM/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ORM
+//
+//  Created by JamesCheng on 2017/3/23.
+//  Copyright © 2017年 Molue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
